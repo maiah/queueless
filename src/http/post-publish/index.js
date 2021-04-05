48b12816e6d037f04d80477de364adb32d278364
Jeremiah Macariola <maiah@starpay.com.ph>
2021-04-05 19:30:01 +0800

    do publish

--- src/http/post-publish/index.js
+++ src/http/post-publish/index.js
@@ -4,11 +4,8 @@ exports.handler = async function http(req) {
   const name = "fninvoker-event";
   const payload = arc.http.helpers.bodyParser(req);
   console.log(`===== ${name}`, payload);
-  // await arc.events.publish({ name, payload });
+  await arc.events.publish({ name, payload });
   return {
-    statusCode: 302,
-    headers: {
-      location: "/",
-    },
+    statusCode: 201,
   };
 };
