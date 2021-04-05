6141c34ba1117f17bc4c2b25caed16078a73269f
Jeremiah Macariola <maiah@starpay.com.ph>
2021-04-05 22:39:49 +0800

    refactor

--- src/events/fninvoker-event/index.js
+++ src/events/fninvoker-event/index.js
@@ -2,7 +2,8 @@ const arc = require("@architect/functions");
 const axios = require("axios");
 
 exports.handler = arc.events.subscribe(async function fninvokerEvent(event) {
-  console.log("invoking:", JSON.parse(event.url));
+  console.log("invoking:", event.url);
+  console.log("parsing:", JSON.parse(event.url));
   await axios.post(event.url, event.payload);
   console.log("successfully invoked:", event.url);
 });
