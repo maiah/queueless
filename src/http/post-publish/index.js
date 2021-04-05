c5dbcf2a9eea71e44acd4e1ef1d2ccd78c43846b
Jeremiah Macariola <maiah@starpay.com.ph>
2021-04-05 22:21:25 +0800

    removed console.log

--- src/http/post-publish/index.js
+++ src/http/post-publish/index.js
@@ -4,7 +4,6 @@ exports.handler = async function http(req) {
   const name = "fninvoker-event";
   const payload = arc.http.helpers.bodyParser(req);
 
-  console.log(`===== ${name}`, payload);
   await arc.events.publish({ name, payload });
 
   return {
